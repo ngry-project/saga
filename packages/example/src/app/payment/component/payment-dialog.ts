e6af851ce6afff705a436dfaf9e1937a86e577b9
achugaev <achugaev93@gmail.com>
2021-10-06 21:02:00 +0300

    chore: fix formatting

--- packages/example/src/app/payment/component/payment-dialog.ts
+++ packages/example/src/app/payment/component/payment-dialog.ts
@@ -2,8 +2,6 @@ import { Observable, of } from 'rxjs';
 import { PaymentDto } from '../dto/payment.dto';
 
 export class PaymentDialog {
-  constructor() {
-  }
 
   afterClosed(): Observable<PaymentDto | null> {
     return of({
