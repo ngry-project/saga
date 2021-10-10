42f83f6e406f0673c162c5a80d0e45ff858e49fb
achugaev <achugaev93@gmail.com>
2021-10-10 19:33:46 +0300

    feat(saga): command handlers return events
    Closes #7
    
    BREAKING CHANGE: public API changed

--- packages/demo-app/src/app/balance/command/balance-top-up.handler.ts
+++ packages/demo-app/src/app/balance/command/balance-top-up.handler.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { EventBus, ICommandHandler } from '@ngry/saga';
+import { ICommandHandler, IEvent } from '@ngry/saga';
 import { BalanceTopUpDoneEvent } from '../event/balance-top-up-done.event';
 import { BalanceTopUpFailEvent } from '../event/balance-top-up-fail.event';
 import { BalanceTopUpDialog } from '../component/balance-top-up-dialog';
@@ -14,19 +14,19 @@ import { BalanceTopUpCommand } from './balance-top-up.command';
 export class BalanceTopUpHandler implements ICommandHandler<BalanceTopUpCommand> {
   readonly executes = BalanceTopUpCommand;
 
-  constructor(private readonly balanceService: BalanceService, private readonly eventBus: EventBus) {}
+  constructor(private readonly balanceService: BalanceService) {}
 
-  execute(command$: Observable<BalanceTopUpCommand>): Observable<unknown> {
+  execute(command$: Observable<BalanceTopUpCommand>): Observable<IEvent> {
     return command$.pipe(
       switchMap((command) =>
         new BalanceTopUpDialog(command.initialAmount).afterClosed().pipe(
           switchMap((form) =>
             this.balanceService.topUp(form.amount).pipe(
-              tap((result) => {
+              map((result) => {
                 if (result) {
-                  this.eventBus.publish(new BalanceTopUpDoneEvent(form.amount, command.context));
+                  return new BalanceTopUpDoneEvent(form.amount, command.context);
                 } else {
-                  this.eventBus.publish(new BalanceTopUpFailEvent(form.amount, command.context));
+                  return new BalanceTopUpFailEvent(form.amount, command.context);
                 }
               }),
             ),
