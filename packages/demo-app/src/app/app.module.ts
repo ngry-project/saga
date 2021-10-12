0f37f4747da6802afdeefabcf490964b10c38a9d
achugaev <achugaev93@gmail.com>
2021-10-13 01:16:13 +0300

    fix(demo-app): fix router imports

--- packages/demo-app/src/app/app.module.ts
+++ packages/demo-app/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { SagaModule } from '@ngry/saga';
 import { PaymentModule } from './payment/payment.module';
 import { BalanceModule } from './balance/balance.module';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, SagaModule.forRoot(), BalanceModule, PaymentModule, RouterModule],
+  imports: [BrowserModule, RouterModule.forRoot([]), SagaModule.forRoot(), BalanceModule, PaymentModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
